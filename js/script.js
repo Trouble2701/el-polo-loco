f5bf2e9917b60dcc98ab275358ebcd9f22a10c46
Sven Friedrich <128075628+Trouble2701@users.noreply.github.com>
2024-04-13 14:19:44 +0200

    optimized window resize

--- js/script.js
+++ js/script.js
@@ -2,92 +2,97 @@ let canvas;
 let world;
 let keyboard = new Keyboard();
 
-function init(){
+function init() {
+    windowRezise();
     canvas = document.getElementById('canvas');
     world = new World(canvas, keyboard);
 }
 
 window.addEventListener('keydown', (event) => {
-    if(event.keyCode == 37){
+    if (event.keyCode == 37) {
         keyboard.left = true;
     }
 
-    if(event.keyCode == 39){
+    if (event.keyCode == 39) {
         keyboard.right = true;
     }
 
-    if(event.keyCode == 32){
+    if (event.keyCode == 32) {
         keyboard.space = true;
     }
 
-    if(event.keyCode == 83){
+    if (event.keyCode == 83) {
         keyboard.shoot = true;
     }
 });
 
 window.addEventListener('keyup', (event) => {
-    if(event.keyCode == 37){
+    if (event.keyCode == 37) {
         keyboard.left = false;
     }
 
-    if(event.keyCode == 39){
+    if (event.keyCode == 39) {
         keyboard.right = false;
     }
 
-    if(event.keyCode == 32){
+    if (event.keyCode == 32) {
         keyboard.space = false;
     }
 
-    if(event.keyCode == 83){
+    if (event.keyCode == 83) {
         keyboard.shoot = false;
     }
 });
 
-function mouseDown(key){
-    if(key == 'left'){
+function mouseDown(key) {
+    if (key == 'left') {
         keyboard.left = true;
     }
 
-    if(key == 'right'){
+    if (key == 'right') {
         keyboard.right = true;
     }
 
-    if(key == 'Jump'){
+    if (key == 'Jump') {
         keyboard.space = true;
     }
 
-    if(key == 'shoot'){
+    if (key == 'shoot') {
         keyboard.shoot = true;
     }
 }
 
-function mouseUp(key){
-    if(key == 'left'){
+function mouseUp(key) {
+    if (key == 'left') {
         keyboard.left = false;
     }
 
-    if(key == 'right'){
+    if (key == 'right') {
         keyboard.right = false;
     }
 
-    if(key == 'Jump'){
+    if (key == 'Jump') {
         keyboard.space = false;
     }
-    if(key == 'shoot'){
+    if (key == 'shoot') {
         keyboard.shoot = false;
     }
 }
 
-window.addEventListener("resize", function () {
+function windowRezise() {
     let width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
     let height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
     if (width <= 1001) {
-        if(height > width){
+        if (height > width) {
             document.getElementById('landscape').style.display = 'flex';
-        }else{
+        } else {
             document.getElementById('landscape').style.display = 'none';
         }
-    }else{
+    } else {
         document.getElementById('landscape').style.display = 'none';
     }
+}
+
+window.addEventListener("resize", function () {
+    windowRezise();
 });
